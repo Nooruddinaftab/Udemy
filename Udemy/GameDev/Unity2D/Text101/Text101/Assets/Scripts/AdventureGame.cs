9d696eef150508ac468d095bff210e26397883eb
nooruddin_pc <noori_dar@yahoo.com>
2023-11-10 16:58:31 +0500

    section 3 lect 21

---
+++ Udemy/GameDev/Unity2D/Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AdventureGame : MonoBehaviour {
+
+	[SerializeField] Text storyTextComponent;
+	[SerializeField] Text titleTextComponent;
+
+	// Use this for initialization
+	void Start () {
+		titleTextComponent.text = "phunk funk Game";
+		//titleTextComponent.fontSize = 21;
+		storyTextComponent.text = "set in program";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
