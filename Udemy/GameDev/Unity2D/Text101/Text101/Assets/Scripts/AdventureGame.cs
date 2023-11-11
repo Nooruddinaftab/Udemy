72c4d113ecf941ef44e48a44d42703bd956ff05e
nooruddin_pc <noori_dar@yahoo.com>
2023-11-11 09:42:08 +0500

    almost completed

--- Udemy/GameDev/Unity2D/Text101/Text101/Assets/Scripts/AdventureGame.cs
+++ Udemy/GameDev/Unity2D/Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,17 +8,39 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 
 	[SerializeField] Text storyTextComponent;
-	[SerializeField] Text titleTextComponent;
+	[SerializeField] State startingState;
+
+	State currentState;
 
 	// Use this for initialization
 	void Start () {
-		titleTextComponent.text = "phunk funk Game";
-		//titleTextComponent.fontSize = 21;
-		storyTextComponent.text = "set in program";
+		currentState = startingState;
+		storyTextComponent.text = currentState.GetStateStory();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ManageState();
+	}
+
+	private void ManageState()
+	{
+		var nextStates = currentState.GetNextStates();
+
+		if (nextStates.Length >= 1 && Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			currentState = nextStates[0];
+		}
+		else if (nextStates.Length >= 2 && (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Q)))
+        {
+			currentState = nextStates[1];
+
+		}
+		else if (nextStates.Length >= 3 && Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			currentState = nextStates[2];
+		}
+
+		storyTextComponent.text = currentState.GetStateStory();
 	}
 }
