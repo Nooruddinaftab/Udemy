f4a5bf34bd7bb89a7c8c8734ff78bbef82ba7401
nooruddin_pc <noori_dar@yahoo.com>
2023-12-06 01:47:32 +0500

    added new level, added sound effect to ball

---
+++ Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+
+	[SerializeField] Paddle paddle1;
+    [SerializeField] float launchVectorX = 2f;
+    [SerializeField] float launchVectorY = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+
+	Vector2 paddleToBallVector;
+    bool hasLaunched = false;
+
+
+    AudioSource myAudioSource;
+	// Use this for initialization
+	void Start () 
+	{
+        hasLaunched = false;
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        if(!hasLaunched)
+        {
+            LockBallToPaddle();
+		    LaunchOnMouseClick();
+        }
+           
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(launchVectorX, launchVectorY);
+            hasLaunched = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(hasLaunched)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+
+            myAudioSource.PlayOneShot(clip);
+
+        }
+    }
+}
---
+++ Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+
+	[SerializeField] float screenWidthInUnits = 16f;
+	[SerializeField] float minPosOnScreenInUnits = 1f;
+	[SerializeField] float maxPosOnScreenInUnits = 15f;
+
+	Vector2 mousePos;
+	float mousePosInUnits = 0f;
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnits;
+		//Debug.Log(mousePosInUnits);
+
+		//mousePos = new Vector2((Input.mousePosition.x / Screen.width) * screenWidthInUnits, paddleTransform.position.y);
+		mousePos.x = Mathf.Clamp(mousePosInUnits, minPosOnScreenInUnits, maxPosOnScreenInUnits);
+		mousePos.y = transform.position.y;
+		transform.position = mousePos;
+	}
+}
