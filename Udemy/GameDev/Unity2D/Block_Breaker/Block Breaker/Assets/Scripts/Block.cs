e78307891b921249c25006311fd3f74c4477f85b
nooruddin_pc <noori_dar@yahoo.com>
2023-12-06 05:02:09 +0500

    added score mechanism

--- Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+    GameStatus gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
+
+        level.AddCountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        DestroyBlock();
+        
        // Debug.Log(collision.gameObject.name);
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        gameStatus.AddToScore();
     }
 }
---
+++ Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+	[SerializeField] int breakableBlocks; // serialized for debugging purposes
+
+	// cached ref
+	SceneLoader sceneLoader;
+
+	public void Start()
+    {
+		sceneLoader = FindObjectOfType<SceneLoader>();
+	}
+	public void AddCountBreakableBlocks()
+    {
+		breakableBlocks++;
+    }
+
+	public void BlockDestroyed()
+	{
+		breakableBlocks--;
+
+		if (breakableBlocks <= 0)
+		{
+			// load next level
+			sceneLoader.LoadNextScene();
+		}
+	}
+
+}
