91ab090bc95537be6ac2104db19a85c164a967c1
nooruddin_pc <noori_dar@yahoo.com>
2023-12-06 20:19:38 +0500

    added particle fx, multiple hit blocks

--- Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Udemy/GameDev/Unity2D/Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -4,32 +4,72 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    // config params
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockParticleEffect;
+    [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     // cached reference
     Level level;
     GameSession gameSession;
 
+    // state variables
+    [SerializeField] int timesHit; // only for debugging
+
+
     private void Start()
     {
         level = FindObjectOfType<Level>();
         gameSession = FindObjectOfType<GameSession>();
-
-        level.AddCountBreakableBlocks();
+        if (gameObject.tag == "Breakable")
+            level.AddCountBreakableBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestroyBlock();
-        
-       // Debug.Log(collision.gameObject.name);
-    }
+        if(gameObject.tag == "Breakable")
+        {
+            HandleHit();
+           
+        }
 
+        // Debug.Log(collision.gameObject.name);
+    }
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        PlayDestroyAudio();
+        TriggerParticleEffect();
         Destroy(gameObject);
         level.BlockDestroyed();
         gameSession.AddToScore();
+    }
+
+    private void PlayDestroyAudio()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+    private void TriggerParticleEffect()
+    {
+        GameObject particleEffect = Instantiate(blockParticleEffect, transform.position, transform.rotation);
+        Destroy(particleEffect, 3);
+
     }
 }
