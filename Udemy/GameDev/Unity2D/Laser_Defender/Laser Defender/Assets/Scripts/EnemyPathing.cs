a873b364b1f53f90afdf61ede46f4e2ee0b9561b
nooruddin_pc <noori_dar@yahoo.com>
2023-12-10 07:17:41 +0500

    laser defender added path for enemy

---
+++ Udemy/GameDev/Unity2D/Laser_Defender/Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypointList;
+    [SerializeField] float moveSpeed = 2f;
+
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypointList[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        if (waypointIndex <= (waypointList.Count - 1))
+        {
+            var targetPosition = waypointList[waypointIndex].transform.position;
+            var movementThisFmae = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFmae);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
