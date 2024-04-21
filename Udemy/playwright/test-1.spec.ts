0d801859c8de6799cfc37f70716dfa291f46a862
nooruddin_pc <noori_dar@yahoo.com>
2024-04-21 16:01:19 +0500

    test123

--- Udemy/playwright/test-1.spec.ts
+++ Udemy/playwright/test-1.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
+test("test", async ({ page }) => {
   // Recording...
+  //something new added
 });
