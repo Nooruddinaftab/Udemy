666c6ca3277e6879f4e4321f674feaca2a460cba
nooruddin_pc <noori_dar@yahoo.com>
2024-05-23 09:34:00 +0500

    test playwright

---
+++ PlaywrightSessions/tests/example.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+
+test("test asdasd Name",
+  {
+    tag: ["@smoke", "@2344"],
+    annotation: {
+      description: "test1",
+      type: "smoke",
+    },
+  },
+  async ({ page, request, context }) => {
+    await page.goto('https://playwright.dev/');
+    let myVar = "adasdasd";
+    await expect(page).toHaveTitle(/Playwright/);
+
+  }
+);
+test("test 223 Name",
+  {
+    tag: ["@smoke", "@2344"],
+    annotation: {
+      description: "test1",
+      type: "smoke",
+    },
+  },
+  async ({ page, request, context }) => {
+    await page.goto('https://playwright.dev/');
+    let myVar = "adasdasd";
+    await expect(page).toHaveTitle(/Playwright/);
+
+  }
+);
